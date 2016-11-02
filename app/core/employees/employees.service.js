13419cabe015d05d922b48ec401e2585a8ad2693
AlexMarvelo <mironenkoalexey@gmail.com>
2016-11-02 14:54:37 +0200

    fix employee service methods security

--- app/core/employees/employees.service.js
+++ app/core/employees/employees.service.js
@@ -4,7 +4,10 @@ angular.
   module('core.employees').
   factory('Employees', ['$log', '$resource', 'Notifications', 'CONFIG',
     function($log, $resource, Notifications, CONFIG) {
-      const serverRequest = $resource(`${CONFIG.env == 'production' ? CONFIG.appDomain_remote : CONFIG.appDomain_local}/employee/:action`, {action: 'get'}, {
+      this.employees = [];
+
+
+      this.serverRequest = $resource(`${CONFIG.env == 'production' ? CONFIG.appDomain_remote : CONFIG.appDomain_local}/employee/:action`, {action: 'get'}, {
         get: {
           method: 'GET',
           params: {
@@ -13,53 +16,55 @@ angular.
         }
       });
 
-      this.employees = [];
 
-
-      serverRequest.get(res => {
-        Notifications.add(res.status);
-        if (res.status == Notifications.codes.success) {
-          this.employees = res.employees || [];
-          $log.debug(`- employees fetched. amount: ${this.employees.length}`);
-        } else {
-          $log.error(res);
+      this.serverRequest.get(response => {
+        if (response.status != Notifications.codes.success) {
+          Notifications.add(response.status);
+          if (CONFIG.consoleErrors) $log.error(response);
+          return;
         }
+        this.employees = response.employees || [];
+        Notifications.add(Notifications.codes.success);
+        $log.debug(`- employees fetched. amount: ${this.employees.length}`);
       });
 
 
-      const get = () => this.employees;
+      this.get = () => this.employees;
 
 
-      const getActiveEmployee = () => this.activeEmployee;
+      this.getActiveEmployee = () => this.activeEmployee;
 
 
-      const setActiveEmployee = (activeEmployee) => {
-        if (!activeEmployee) {
-          if (this.activeEmployee) $log.debug('- unset active employee');
-          this.activeEmployee = undefined;
-          return;
+      this.setActiveEmployee = (activeEmployee) => {
+        try {
+          if (!activeEmployee) {
+            if (this.activeEmployee) $log.debug('- unset active employee');
+            this.activeEmployee = undefined;
+            return;
+          }
+          if (!activeEmployee.id) {
+            throw { status: Notifications.codes.idRequired };
+          }
+          const targetEmployee = this.employees.find(employee => employee.id == activeEmployee.id);
+          if (!targetEmployee) {
+            throw { status: Notifications.codes.employeeNotFound };
+          }
+          this.activeEmployee = targetEmployee;
+          $log.debug(`- set active employee to ${this.activeEmployee.id}` +
+            `${this.activeEmployee.firstName && this.activeEmployee.lastName ? ' (' + this.activeEmployee.firstName + ' ' + this.activeEmployee.lastName + ')' : ''}`
+          );
+        } catch (error) {
+          Notifications.add(error);
+          if (CONFIG.consoleErrors) $log.error(error);
         }
-        if (activeEmployee.id == undefined) {
-          Notifications.add(Notifications.codes.idRequired);
-          return;
-        }
-        const targetEmployee = this.employees.find(employee => employee.id == activeEmployee.id);
-        if (!targetEmployee) {
-          Notifications.add(Notifications.codes.employeeNotFound);
-          return;
-        }
-        this.activeEmployee = targetEmployee;
-        $log.debug(`- set active employee to ${this.activeEmployee.id}` +
-          `${this.activeEmployee.firstName && this.activeEmployee.lastName ? ' (' + this.activeEmployee.firstName + ' ' + this.activeEmployee.lastName + ')' : ''}`
-        );
       };
 
 
       return {
-        get,
-        serverRequest,
-        getActiveEmployee,
-        setActiveEmployee
+        get: this.get,
+        serverRequest: this.serverRequest,
+        getActiveEmployee: this.getActiveEmployee,
+        setActiveEmployee: this.setActiveEmployee,
       };
     }
   ]);
