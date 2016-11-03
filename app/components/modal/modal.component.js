27c8bfab295e6135616dab0e8f4b4ce6c1394e77
AlexMarvelo <mironenkoalexey@gmail.com>
2016-11-03 21:18:34 +0200

    fix modal async methods

--- app/components/modal/modal.component.js
+++ app/components/modal/modal.component.js
@@ -311,8 +311,13 @@ angular.
                 employee.lastName.toLowerCase().indexOf(query) != -1 ||
                 (employee.firstName.toLowerCase() + ' ' + employee.lastName.toLowerCase()).indexOf(query) != -1
               )
-              .map(employee => `<li data-id="${employee.id}">${employee.firstName} ${employee.lastName}</li>`)
-              .sort();
+              .sort((a, b) => {
+                if (a.firstName < b.firstName) return -1;
+                if (a.firstName > b.firstName) return 1;
+                return 0;
+              })
+              .map(employee => `<li data-id="${employee.id}">${employee.firstName} ${employee.lastName}</li>`);
+
             form.find('.glyphicon-remove--employee').remove();
             employeeList.html(query.length ? newList.join('') : '');
           });
@@ -378,9 +383,9 @@ angular.
                   tooltipTitle: newEmployee ? `${newEmployee.firstName} ${newEmployee.lastName}` : undefined
                 }));
                 this.modal.iziModal('close');
-              }, () => {});
-          }, () => {})
-          .catch(error => $log.error(error));
+              }, () => {})
+              .catch(error => $log.error(error));
+          }, () => {});
         };
 
 
@@ -396,9 +401,9 @@ angular.
               .then(() => {
                 this.mapcanvas.removeSeat(seat);
                 this.modal.iziModal('close');
-              }, () => {});
-          }, () => {})
-          .catch(error => $log.error(error));
+              }, () => {})
+              .catch(error => $log.error(error));
+          }, () => {});
         };
 
 
@@ -416,9 +421,9 @@ angular.
                 this.mapcanvas.updateSeat(this.seat.id, Object.assign({}, this.seat, {
                   tooltipTitle: undefined
                 }));
-              }, () => {});
-          }, () => {})
-          .catch(error => $log.error(error));
+              }, () => {})
+              .catch(error => $log.error(error));
+          }, () => {});
         };
 
 
@@ -442,7 +447,7 @@ angular.
           let valid = true;
           let seatID = this.modal.find('input[name="seatID"]').val();
           if (seatID == this.seat.id) return valid;
-          valid = Floor(floorID).getSeats().find(seat => seat.id == seatID) == undefined;
+          valid = this.seats ? this.seats.find(seat => seat.id == seatID) == undefined : true;
           if (!valid) {
             this.addNotification({
               status: 'ERROR',
@@ -629,21 +634,23 @@ angular.
             })
             .then(() => {
               if (seatID.length) {
-                $scope.$apply(() => {
-                  Floor(floorID).attachEmployeeToSeat(seatID, this.employee.id);
-                });
-                this.mapcanvas.setSeatTooltipTitle(seatID, `${this.employee.firstName} ${this.employee.lastName}`);
-                this.mapcanvas.activateOneSeat({id: seatID});
+                Floor(floorID).attachEmployeeToSeat(seatID, this.employee.id)
+                  .then(() => {
+                    this.mapcanvas.setSeatTooltipTitle(seatID, `${this.employee.firstName} ${this.employee.lastName}`);
+                    this.mapcanvas.activateOneSeat({id: seatID});
+                    this.modal.iziModal('close');
+                  }, () => {})
+                  .catch(error => $log.error(error));
               } else {
-                $scope.$apply(() => {
-                  Floor(floorID).attachEmployeeToSeat(this.employee.seatID, undefined);
-                });
-                this.mapcanvas.setSeatTooltipTitle(this.employee.seatID, undefined);
-                this.mapcanvas.deactivateAllSeats();
+                Floor(floorID).attachEmployeeToSeat(this.employee.seatID, undefined)
+                  .then(() => {
+                    this.mapcanvas.setSeatTooltipTitle(this.employee.seatID, undefined);
+                    this.mapcanvas.deactivateAllSeats();
+                    this.modal.iziModal('close');
+                  }, () => {})
+                  .catch(error => $log.error(error));
               }
-              this.modal.iziModal('close');
-            }, () => {})
-            .catch(error => $log.error(error));
+            }, () => {});
           });
         };
 
@@ -659,7 +666,8 @@ angular.
               }
               this.pasteEmployeeSeatBlock();
               this.mapcanvas.activateOneSeat(this.employee.seat);
-            }, () => {});
+            }, () => {})
+            .catch(error => $log.error(error));
         };
 
 
@@ -716,8 +724,12 @@ angular.
                 seat.id.toLowerCase().indexOf(query) != -1 ||
                 (seat.title && seat.title.toLowerCase().indexOf(query) != -1)
               )
-              .map(seat => `<li data-id="${seat.id}">${seat.title || seat.id}</li>`)
-              .sort();
+              .sort((a, b) => {
+                if (a.id < b.id) return -1;
+                if (a.id > b.id) return 1;
+                return 0;
+              })
+              .map(seat => `<li data-id="${seat.id}">${seat.title || seat.id}</li>`);
             form.find('.glyphicon-remove--seat').remove();
             seatList.html(query.length ? newList.join('') : '');
           });
@@ -774,14 +786,15 @@ angular.
             msg: questionText
           })
           .then(() => {
-            $scope.$apply(() => {
-              Floor(floorID).attachEmployeeToSeat(this.employee.seat.id, undefined);
-            });
-            this.mapcanvas.setSeatTooltipTitle(this.employee.seat.id, undefined);
-            this.employee.seat = undefined;
-            this.clearSeatsList();
-          }, () => {})
-          .catch(error => $log.error(error));
+            Floor(floorID).attachEmployeeToSeat(this.employee.seat.id, undefined)
+              .then(() => {
+                this.mapcanvas.setSeatTooltipTitle(this.employee.seat.id, undefined);
+                this.mapcanvas.deactivateAllSeats();
+                this.employee.seat = undefined;
+                this.clearSeatsList();
+              }, () => {})
+              .catch(error => $log.error(error));
+          }, () => {});
         };
 
 
